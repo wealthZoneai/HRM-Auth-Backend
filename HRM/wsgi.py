1e152ccf11f102213cd4f376869b0d50112da651
wealthZoneai <smarty.chakri@gmail.com>
2025-12-04 17:41:18 +0530

    grading

--- HRM/wsgi.py
+++ HRM/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
+# Set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HRM.settings')
 
-application = get_wsgi_application()
+# Get the Django WSGI application
+django_app = get_wsgi_application()
+
+# Wrap with WhiteNoise for static files
+application = WhiteNoise(
+    django_app,
+    root=os.path.join(Path(__file__).resolve().parent.parent, 'staticfiles'),
+    prefix='static/'
+)
+
+# Add additional directories to the WhiteNoise application
+application.add_files(os.path.join(Path(__file__).resolve().parent.parent, 'static'), prefix='static/')
